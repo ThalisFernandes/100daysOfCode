fa5bedf57d0b4097667b9406529da6c7a5a9f6af
ThalisFernandes <thalisfernandesrodrigues@gmail.com>
2022-09-27 14:07:03 -0300

    feat: dia 1 do desafio de 100dias de código reduce-filter-map

---
+++ day1.js
@@ -0,0 +1,37 @@
+/**
+ * ##### DIA 1 #######
+ * 
+ * ME APROFUNDANDO MAIS EM METHODS PARA ARRAY EM JAVASCRIPT
+ * 
+ * 
+ * REDUCE() 
+ * 
+ * sintaxe: 
+ *   o reduce recebe uma função de callback para retornar o acumulador que é um parametro padrao 
+ *  
+ *  array.reduce((acc, vlrAtual)=>{
+ *          
+ *  }, vlrInicial);
+ * 
+ */
+
+
+
+// contando elementos repetidos do array com reduce
+
+
+let nomes = ['maria', "mara", 'paulo', 'jose','jose', 'maria','mara','julia', 'pedro', 'antonio', 'jose'];
+
+let repeticoes = nomes.reduce((acc, vlrAtual)=> {
+    if(nomes[vlrAtual] in acc){
+        acc[vlrAtual] ++
+    } else{
+        acc[vlrAtual] = 1 
+    }
+    return acc 
+}, {});
+
+/**
+ * no retorno desse reduce deve retornar um dicionario contendo os nomes e a quantidade de repeticoes de cada nome
+ * {'maria': 2, 'mara': 2 ...};
+*/
